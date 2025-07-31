8d739cc2e3015faf4dfaf7b20521d832276dcc03
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-07-31 10:42:01 +0300

    adding ngModel to inputs

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
 })
 export class NewTask {
   @Output() cancel = new EventEmitter<void>();
-  
+  enteredTitle= '';  // property to store newly added title by user input  // we add ngModel in html input field
+                    //  can be used as signals also    
   onCancel() {
     this.cancel.emit(); // Emit the cancel event when the user cancels task addition
   }
